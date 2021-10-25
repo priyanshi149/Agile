ed084572181c20b948f3750b8fc41b9957d35c5d
priyanshi149 <66512593+priyanshi149@users.noreply.github.com>
2021-10-25 13:03:16 +0530

    Add files via upload

---
+++ Unittest.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestC(unittest.TestCase):
+
+    #def test_add(self):
+        #result = math.calculator(10,5)
+        #self.assertEqual(result,15)
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()        
+
