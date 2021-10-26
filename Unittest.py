3471569c592747e3a501442d8013e3c38186d695
priyanshi149 <66512593+priyanshi149@users.noreply.github.com>
2021-10-26 10:28:09 +0530

    Update Unittest.py

--- Unittest.py
+++ Unittest.py
@@ -6,7 +6,7 @@ class Test(unittest.TestCase):
     def test_add(self):
         a=15
         self.assertEqual(a,15)
-        self.assertEqual(a,10)
+       #self.assertEqual(a,10)
 if __name__ == '__main__':
     unittest.main()        
     
